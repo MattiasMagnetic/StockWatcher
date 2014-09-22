39e49a433ec090bff700f72f6a9acb3c5222b885
Mattias Martens <mattiasofmartens@gmail.com>
2014-09-22 11:30:34 -0700

    Change again

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -238,7 +238,7 @@ private void displayStocks(String[] symbols) {
 
       StockPrice[] prices = new StockPrice[stocks.size()];
       for (int i = 0; i < stocks.size(); i++) {
-        double price = (Math.sin(Random.nextDouble()) +1)/2 * MAX_PRICE;
+        double price = (Random.nextDouble() + 1 / 2) * MAX_PRICE;
         double change = price * MAX_PRICE_CHANGE
             * (Random.nextDouble() * 2.0 - 1.0);
 
